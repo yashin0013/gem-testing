b1ca99369c24beac5a280a28255763d4d1b814d8
Yashin <yashin123786@gmail.com>
2024-03-08 15:35:54 +0530

    Modal UI improved

--- resources/views/gem/details.blade.php
+++ resources/views/gem/details.blade.php
@@ -2,53 +2,53 @@
   <div class="row">
     <div class="col-md-8">
       <table class="table">
-          <tbody>
-            <tr>
-              <th scope="row">Report Number</th>
-              <td>{{$gem->report_number}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Weight of stone</th>
-              <td>{{$gem->weight}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Dimension</th>
-              <td colspan="2">{{$gem->dimension}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Color</th>
-              <td colspan="2">{{$gem->color}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Shape & Cut</th>
-              <td colspan="2">{{$gem->shape_cut}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Optic Character</th>
-              <td colspan="2">{{$gem->optic_char}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Refractive Index</th>
-              <td colspan="2">{{$gem->refractive_index}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Specific Gravity</th>
-              <td colspan="2">{{$gem->specific_gravity}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Microscope View</th>
-              <td colspan="2">{{$gem->microscope_view}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Species</th>
-              <td colspan="2">{{$gem->species}}</td>
-            </tr>
-            <tr>
-              <th scope="row">Comments</th>
-              <td colspan="2">{{$gem->comments}}</td>
-            </tr>
-          </tbody>
-        </table>
+        <tbody>
+          <tr>
+            <th scope="row">Report Number</th>
+            <td>{{$gem->report_number}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Weight of stone</th>
+            <td>{{$gem->weight}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Dimension</th>
+            <td colspan="2">{{$gem->dimension}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Color</th>
+            <td colspan="2">{{$gem->color}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Shape & Cut</th>
+            <td colspan="2">{{$gem->shape_cut}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Optic Character</th>
+            <td colspan="2">{{$gem->optic_char}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Refractive Index</th>
+            <td colspan="2">{{$gem->refractive_index}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Specific Gravity</th>
+            <td colspan="2">{{$gem->specific_gravity}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Microscope View</th>
+            <td colspan="2">{{$gem->microscope_view}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Species</th>
+            <td colspan="2">{{$gem->species}}</td>
+          </tr>
+          <tr>
+            <th scope="row">Comments</th>
+            <td colspan="2">{{$gem->comments}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <div class="col-md-4">
       <div class="gem-img-box">
@@ -72,17 +72,19 @@
             <input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
           </div>
 
-        <div class="mb-3 col-6">
-          <input type="phone" class="form-control" id="phone" name="phone" placeholder="Your Phone Number">
+          <div class="mb-3 col-6">
+            <input type="phone" class="form-control" id="phone" name="phone" placeholder="Your Phone Number">
+          </div>
+          <div class="mb-3 col-6">
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+          </div>
+          <div class="mb-3 col-12">
+            <textarea class="form-control" name="message" placeholder="Type your message here"></textarea>
+          </div>
+          <div class="col-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
         </div>
-        <div class="mb-3 col-6">
-          <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-        </div>
-        <div class="mb-3 col-12">
-          <textarea class="form-control" name="message" placeholder="Type your message here"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
       </form>
     </div>
   </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 
   <!-- Modal -->
   <div class="modal fade" id="reportModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-dialog modal-dialog-centered modal-xl modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Natural Blue Sapphire Gemstone</h1>
